d23c063583ca740afe8555c5d4f590736972fc3e
Ryan Gillespie <ryan.gillespie@tuta.io>
2020-07-30 04:33:48 -0700

    initial commit

---
+++ mybad/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mybad
+//
+//  Created by Ryan Gillespie on 7/30/20.
+//  Copyright © 2020 48M7F74MVK. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <AppleScriptObjC/AppleScriptObjC.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    [[NSBundle mainBundle] loadAppleScriptObjectiveCScripts];
+    return NSApplicationMain(argc, argv);
+}
